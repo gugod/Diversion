decaa3734d8bd02ba85c104a3f57a3d609e501da
Kang-min Liu <gugod@gugod.org>
2013-07-17 23:34:38 +0200

    update the archiver to properly store feed entries with update_at/created_at info.

--- lib/Diversion/FeedArchiver.pm
+++ lib/Diversion/FeedArchiver.pm
@@ -1,7 +1,6 @@
 package Diversion::FeedArchiver {
     use Moo;
     use ElasticSearch;
-    use Encode;
     use HTML::Restrict;
 
     use Diversion::FeedFetcher;
@@ -29,50 +28,92 @@ package Diversion::FeedArchiver {
         return ElasticSearch->new();
     }
 
+    sub log {
+        my ($self, @args) = @_;
+        print STDERR @args,"\n";
+    }
+
+    sub create_index_unless_exists {
+        my ($self) = @_;
+        return if $self->elasticsearch->index_exists(index => "diversion");
+        $self->elasticsearch->create_index(index => "diversion");
+    }
+
     sub run {
         my ($self) = @_;
 
-        my @bulk_actions;
+        $self->create_index_unless_exists;
+
+        my %bulk_actions;
 
         $self->fetcher->each_entry(
             sub {
                 my ($entry, $i) = @_;
 
-                my $data = {
-                    created_at => time,
-                };
+                my @t = (gmtime)[5,4,3,2,1,0];
+                $t[0] += 1900;
+                my $now = sprintf("%4d-%02d-%02dT%02d:%02d:%02d", @t);
+
+                my $data = { updated_at =>  $now };
 
                 for (qw(title pubDate author guid author category description)) {
-                    my $v = $entry->$_ or next;
-                    if (ref($v) eq 'HASH' && $v->{'#text'}) {
-                        $v = decode 'utf8', $v->{'#text'};
-                    }
-                    if (ref($v) eq 'ARRAY') {
-                        @$v = map { $_ = decode('utf8', $_) } @$v;
-                    }
-                    unless (ref($v)) {
-                        $v = decode "utf8" => $v;
-                    }
-                    $data->{$_} = $v;
+                    $data->{$_} = $entry->$_;
                 }
 
                 my $stripper = HTML::Restrict->new;
                 $data->{description} = $stripper->process($data->{description});
 
-                push @bulk_actions, {
+                my $entry_link = $entry->link;
+                $bulk_actions{$entry_link} = {
                     index => {
-                        id => $entry->link,
+                        id => $entry_link,
                         data => $data
                     }
                 };
             }
         );
 
-        $self->elasticsearch->bulk(
-            actions => \@bulk_actions,
-            index   => "diversion",
-            type    => "feed_entry"
+        my @entry_urls = keys %bulk_actions;
+
+        my $existing_doc = $self->elasticsearch->mget(
+            index => "diversion",
+            type => "feed_entry",
+            ids => \@entry_urls
         );
+
+        for my $d (@$existing_doc) {
+            if ($d->{exists}) {
+                my $new_is_old = 1;
+                my $new_data = $bulk_actions{$d->{_id}}{index}{data};
+                $new_data->{created_at} = $d->{_source}{created_at};
+                for (keys %{$d->{_source}}) {
+                    next if $_ eq "updated_at";
+                    if (defined($d->{_source}{$_}) && defined($new_data->{$_}) && $d->{_source}{$_} ne $new_data->{$_}) {
+                        $new_is_old = 0;
+                        last;
+                    }
+                }
+                if ($new_is_old) {
+                    delete $bulk_actions{$d->{_id}};
+                }
+            }
+        }
+
+
+        if (my @bulk_actions = values %bulk_actions) {
+            for (@bulk_actions) {
+                $_->{index}{data}{created_at} ||= $_->{index}{data}{updated_at};
+            }
+
+            $self->elasticsearch->bulk(
+                actions => \@bulk_actions,
+                index   => "diversion",
+                type    => "feed_entry"
+            );
+        }
+        else {
+            $self->log("Nothing to update. All feed entries are the same as it was.");
+        }
     }
 };
 
