4ee375f9cc25842f691b3c8af07490c177d74835
Kang-min Liu <gugod@gugod.org>
2015-02-14 22:01:57 +0100

    split the the digest string into multi-level dir.

--- lib/Diversion/BlobStore.pm
+++ lib/Diversion/BlobStore.pm
@@ -8,26 +8,38 @@ has root => (
     required => 1,
 );
 
+has digest_function => (
+    is => "ro",
+    default => sub {
+        return \&sha1_hex
+    }
+);
+
+sub _io {
+    my ($self, $digest) = @_;
+    my @s = grep $_, split /(....)/, $digest;
+    my $o = io->catfile($self->root, @s);
+    return $o;
+}
+
 sub put {
     my ($self, $data) = @_;
-    my $digest = sha1_hex($data);
-
-    my $o = io->catfile($self->root, $digest);
+    my $digest = $self->digest_function->($data);
+    my $o = $self->_io($digest);
     $o->assert->print($data) unless $o->exists;
-
     return $digest;
 }
 
 sub get {
     my ($self, $digest) = @_;
-    my $o = io->catfile($self->root, $digest);
+    my $o = $self->_io($digest);
     return undef unless $o->exists;
     return $o->all;
 }
 
 sub get_path {
     my ($self, $digest) = @_;
-    my $o = io->catfile($self->root, $digest);
+    my $o = $self->_io($digest);
     return undef unless $o->exists;
     return $o->pathname;
 }
