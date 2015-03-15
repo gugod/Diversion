85fcc20b3125fb12cc285aa37aee191e6e69d6ee
Kang-min Liu <gugod@gugod.org>
2015-03-15 13:36:56 +0100

    set config from toml file in an XDG config folder.

--- lib/Diversion/App.pm
+++ lib/Diversion/App.pm
@@ -1,3 +1,15 @@
 package Diversion::App;
 use App::Cmd::Setup -app;
+
+sub set_config {
+    my ($self, $config) = @_;
+    $self->{_app}{config} = $config;
+    return $self;
+}
+
+sub config {
+    my ($self) = @_;
+    return $self->{_app}{config} //= {};
+}
+
 1;
