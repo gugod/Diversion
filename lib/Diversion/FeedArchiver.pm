0a80cc85fa864a833495ee2d64285da28edde815
Kang-min Liu <gugod@gugod.org>
2015-10-18 22:02:31 +0200

    store entry_json in a separate table.

--- lib/Diversion/FeedArchiver.pm
+++ lib/Diversion/FeedArchiver.pm
@@ -37,14 +37,21 @@ package Diversion::FeedArchiver {
         my $JSON = JSON->new->canonical->utf8->pretty;
 
         my $dbh = $self->dbh_index;
-        my $sth_insert = $dbh->prepare(q{ INSERT INTO feed_archive(uri, created_at, entry_sha1_digest) VALUES (?,?,?)});
-        my $sth_check = $dbh->prepare(q{ SELECT 1 FROM feed_archive WHERE uri = ? AND entry_sha1_digest = ? LIMIT 1});
 
-        my $feed_json = $JSON->encode( Diversion::FeedFetcher->new(url => $url)->feed );
-        my $digest = $self->blob_store->put( $feed_json );
+        my $sth_insert = $dbh->prepare(q{ INSERT INTO feed_archive(uri, created_at, sha1_digest) VALUES (?,?,?)});
+        my $sth_check = $dbh->prepare(q{ SELECT 1 FROM feed_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1});
+        my $sth_entry_insert = $dbh->prepare(q{ INSERT INTO feed_entries(uri, created_at, entry_json) VALUES (?,?,?)});
+
+        my $feed = Diversion::FeedFetcher->new(url => $url)->feed;
+        my $digest = $self->blob_store->put( "". $JSON->encode( $feed ) );
+
         $sth_check->execute($url, $digest);
         unless ($sth_check->fetchrow_array) {
             $sth_insert->execute($url, 0+time, $digest);
+            for my $entry (@{ $feed->{entry} }) {
+                my $entry_json = $JSON->encode($entry);
+                $sth_entry_insert->execute($entry->{link}, 0+time, $entry_json);
+            }
         }
     }
 };
