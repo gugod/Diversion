06a824765a1faed4667cd0a94ac7c71060059f7e
Kang-min Liu <gugod@gugod.org>
2015-10-19 00:09:12 +0200

    remove some uses of intermediate variables.

--- lib/Diversion/UrlArchiver.pm
+++ lib/Diversion/UrlArchiver.pm
@@ -10,6 +10,8 @@ package Diversion::UrlArchiver {
 
     with 'Diversion::AppRole';
 
+    my $JSON = JSON->new->canonical->pretty;
+
     has dbh_index =>  (
         is => "ro",
         default => sub {
@@ -24,23 +26,20 @@ package Diversion::UrlArchiver {
 
     sub get {
         my ($self, $url) = @_;
-        my $blob_store = $self->blob_store;
 
-        my $http = HTTP::Tiny->new( timeout => 60 );
-        my $JSON = JSON->new->canonical->pretty;
-        my $response = $http->get($url);
+        my $blob_store = $self->blob_store;
         my $dbh = $self->dbh_index;
+
         my $sth_insert = $dbh->prepare(q{ INSERT INTO uri_archive(uri, created_at, sha1_digest) VALUES (?,?,?)});
         my $sth_check = $dbh->prepare(q{ SELECT 1 FROM uri_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1});
 
+        my $response = HTTP::Tiny->new( timeout => 60 )->get($url);
+
         if (is_binary($response->{content})) {
-            my $content = delete $response->{content};
-            my $content_digest = $blob_store->put($content);
-            $response->{content} = { sha1_digest => $content_digest };
+            $response->{content} = { sha1_digest => $blob_store->put($response->{content}) };
         }
 
-        my $response_json = $JSON->encode($response);
-        my $response_digest = $blob_store->put($response_json);
+        my $response_digest = $blob_store->put("". $JSON->encode($response));
 
         $sth_check->execute($url, $response_digest);
         if ($sth_check->fetchrow_array) {
