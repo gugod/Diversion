3744004ad5eeffee070d70ef9ad6072815fe4636
Kang-min Liu <gugod@gugod.org>
2015-10-20 08:11:17 +0200

    Avoid re-downloading in general.

--- lib/Diversion/UrlArchiver.pm
+++ lib/Diversion/UrlArchiver.pm
@@ -40,28 +40,24 @@ package Diversion::UrlArchiver {
     sub get {
         my ($self, $url) = @_;
 
+        my $response = $self->get_local($url);
+        return $response if defined $response;
+
         my $blob_store = $self->blob_store;
-        my $dbh = $self->dbh_index;
 
-        my $sth_insert = $dbh->prepare(q{ INSERT INTO uri_archive(uri, created_at, sha1_digest) VALUES (?,?,?)});
-        my $sth_check = $dbh->prepare(q{ SELECT 1 FROM uri_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1});
-
-        my $response = HTTP::Tiny->new( timeout => 60 )->get($url);
-
+        $response = HTTP::Tiny->new( timeout => 60 )->get($url);
         if (is_binary($response->{content})) {
             $response->{content} = { sha1_digest => $blob_store->put($response->{content}) };
         }
-
         my $response_digest = $blob_store->put("". $JSON->encode($response));
 
+        my $dbh = $self->dbh_index;
+        my $sth_check = $dbh->prepare(q{ SELECT 1 FROM uri_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1});
         $sth_check->execute($url, $response_digest);
-        if ($sth_check->fetchrow_array) {
-            say "OLD $url";
-        } else {
+        unless ($sth_check->fetchrow_array) {
+            my $sth_insert = $dbh->prepare(q{ INSERT INTO uri_archive(uri, created_at, sha1_digest) VALUES (?,?,?)});
             $sth_insert->execute($url, 0+time, $response_digest);
-            say "STORE $url " . ( ref($response->{content}) ? "#binary" : "" );
         }
-
         return $response;
     }
 };
