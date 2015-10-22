66ae8e0b585527c0f0545746d2e3ffd11bb5a628
Kang-min Liu <gugod@gugod.org>
2015-10-23 00:03:19 +0200

    Add a "--content-type" option for filtering.

--- lib/Diversion/App/Command/cat_url_archive.pm
+++ lib/Diversion/App/Command/cat_url_archive.pm
@@ -1,16 +1,36 @@
 package Diversion::App::Command::cat_url_archive;
 use v5.18;
 use Diversion::App -command;
+
 use Diversion::UrlArchiver;
+use JSON::XS;
+
+sub opt_spec {
+    return (
+        ["content-type=s", "Only the content-type"]
+    )
+}
 
 sub execute {
-    my ($self) = @_;
-    my $dbh = Diversion::UrlArchiver->new->dbh_index;
-    my $rows = $dbh->selectall_arrayref("SELECT distinct uri FROM uri_archive", {Slice=>{}});
-    $dbh->disconnect;
+    my ($self, $opt) = @_;
+    my $o = Diversion::UrlArchiver->new;
+    my $dbh = $o->dbh_index;
+    my $rows = $dbh->selectall_arrayref("SELECT uri,sha1_digest,created_at FROM  uri_archive ORDER BY uri ASC, created_at DESC", {Slice=>{}});
+    my $JSON = JSON::XS->new;
+
+    my $last = "";
     for my $row (@$rows) {
-        say $row->{uri};
+        if ($opt->{content_type}) {
+            my $blob = $o->blob_store->get($row->{sha1_digest});
+            my $res = $JSON->decode( $blob );
+            next unless index($res->{headers}{"content-type"}, $opt->{content_type}) > 0;
+        }
+        if ($last ne $row->{uri}) {
+            say $last = $row->{uri};
+        }
     }
+
+    $dbh->disconnect;
 }
 
 1;
