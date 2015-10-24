25177194bcc62b9f5f3a5dc6d6eb68c5888d1f20
Kang-min Liu <gugod@gugod.org>
2015-10-24 14:38:59 +0200

    cat_url_archive: rewritten with UrlArchiveIterator

--- lib/Diversion/App/Command/cat_url_archive.pm
+++ lib/Diversion/App/Command/cat_url_archive.pm
@@ -1,8 +1,10 @@
 package Diversion::App::Command::cat_url_archive;
 use v5.18;
+use Moo;
+with 'Diversion::AppRole';
 use Diversion::App -command;
 
-use Diversion::UrlArchiver;
+use Diversion::UrlArchiveIterator;
 use JSON::XS;
 
 sub opt_spec {
@@ -13,24 +15,19 @@ sub opt_spec {
 
 sub execute {
     my ($self, $opt) = @_;
-    my $o = Diversion::UrlArchiver->new;
-    my $dbh = $o->dbh_index;
-    my $rows = $dbh->selectall_arrayref("SELECT uri,sha1_digest,created_at FROM  uri_archive ORDER BY uri ASC, created_at DESC", {Slice=>{}});
     my $JSON = JSON::XS->new;
-
     my $last = "";
-    for my $row (@$rows) {
+    my $iter = Diversion::UrlArchiveIterator->new();
+    while (my $row = $iter->next) {
         if ($opt->{content_type}) {
-            my $blob = $o->blob_store->get($row->{sha1_digest});
+            my $blob = $self->blob_store->get($row->{sha1_digest});
             my $res = $JSON->decode( $blob );
-            next unless index($res->{headers}{"content-type"}, $opt->{content_type}) > 0;
+            next unless defined($res->{headers}{"content-type"}) && index($res->{headers}{"content-type"}, $opt->{content_type}) > 0;
         }
         if ($last ne $row->{uri}) {
             say $last = $row->{uri};
         }
     }
-
-    $dbh->disconnect;
 }
 
 1;
