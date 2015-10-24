bc68bd10935d066e0071ed8e06f33848938da9f8
Kang-min Liu <gugod@gugod.org>
2015-10-24 17:55:45 +0200

    Add a command to archive links in OPML files.

---
+++ lib/Diversion/App/Command/archive_opml_links.pm
@@ -0,0 +1,40 @@
+package Diversion::App::Command::archive_opml_links;
+use v5.18;
+use Diversion::App -command;
+use Diversion::FeedArchiver;
+
+use Parallel::ForkManager;
+use Log::Any qw($log);
+use XML::XPath;
+use List::MoreUtils 'uniq';
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+
+    my @feeds;
+    for my $opml_file (@$args) {
+        my $xp = XML::XPath->new(filename => $opml_file);
+        my $resultset = $xp->find('//outline[@xmlUrl]');
+        for my $node ($resultset->get_nodelist) {
+            push @feeds, $node->getAttribute("xmlUrl");
+        }
+    }
+
+
+    my $forkman = Parallel::ForkManager->new(4);
+    for (uniq grep { /^https?:/ } @feeds) {
+        $forkman->start and next;
+        my $feed_archiver = Diversion::FeedArchiver->new;
+        say "[pid=$$] Processing $_";
+        eval {
+            $feed_archiver->fetch_then_archive( $_ );
+            1;
+        } or do {
+            say STDERR $@;
+        };
+        $forkman->finish;
+    }
+    $forkman->wait_all_children;
+}
+
+1;
