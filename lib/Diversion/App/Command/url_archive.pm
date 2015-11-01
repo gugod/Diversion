9bd660b38a699570b658a291f2e888d98db15bb9
Kang-min Liu <gugod@gugod.org>
2015-11-01 11:46:21 +0100

    Process @args in parallel.

--- lib/Diversion/App/Command/url_archive.pm
+++ lib/Diversion/App/Command/url_archive.pm
@@ -2,14 +2,20 @@ package Diversion::App::Command::url_archive;
 use v5.18;
 use Diversion::App -command;
 use Diversion::UrlArchiver;
+use Parallel::ForkManager;
 
 sub execute {
     my ($self, $opt, $args) = @_;
 
+    my $forkman = Parallel::ForkManager->new(4);
     my $o = Diversion::UrlArchiver->new;
     for my $url (@$args) {
+        $forkman->start and next;
         $o->get_remote($url);
+        say "[$$] ARCHIVE $url";
+        $forkman->finish;
     }
+    $forkman->wait_all_children;
 }
 
 1;
