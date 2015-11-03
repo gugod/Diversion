46a8aa0d69a51266e596c9ab25f6b56b67678cc2
Kang-min Liu <gugod@gugod.org>
2015-11-03 18:18:59 +0100

    Add "--workers" option.

--- lib/Diversion/App/Command/url_archive.pm
+++ lib/Diversion/App/Command/url_archive.pm
@@ -4,10 +4,16 @@ use Diversion::App -command;
 use Diversion::UrlArchiver;
 use Parallel::ForkManager;
 
+sub opt_spec {
+    return (
+        ["workers=n", "number of worker processes.", { default => 4 }]
+    )
+}
+
 sub execute {
     my ($self, $opt, $args) = @_;
 
-    my $forkman = Parallel::ForkManager->new(4);
+    my $forkman = Parallel::ForkManager->new($opt->{workers});
     my $o = Diversion::UrlArchiver->new;
     for my $url (@$args) {
         $forkman->start and next;
