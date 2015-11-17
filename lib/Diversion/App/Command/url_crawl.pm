b20bb4768cd8618bf830acfd9e6ac8ff8207b128
Kang-min Liu <gugod@gugod.org>
2015-11-17 08:57:56 +0100

    Add url_crawl command
    This currently crawl 1 level from the entry point.

---
+++ lib/Diversion/App/Command/url_crawl.pm
@@ -0,0 +1,83 @@
+package Diversion::App::Command::url_crawl;
+use v5.18;
+use Diversion::App -command;
+use Moo;
+with 'Diversion::Db';
+use Log::Any qw($log);
+
+use List::Util 'shuffle';
+use Parallel::ForkManager;
+
+sub opt_spec {
+    return (
+        ["workers=n", "number of worker processes.", { default => 4 }]
+    )
+}
+
+require Data::Dumper;
+sub execute {
+    my ($self, $opt, $args) = @_;
+    my $url_archiver = Diversion::UrlArchiver->new;
+
+    my @uris;
+    my $forkman = Parallel::ForkManager->new( $opt->{workers} );
+    $forkman->run_on_finish(
+        sub {
+            my $data = pop;
+            if ($data) {
+                push @uris, @$data;
+            }
+        }
+    );
+
+    for my $u (@$args) {
+        $forkman->start and next;
+        my $response = $url_archiver->get_remote($u);
+        $log->debug("[$$] CRAWL $response->{status} $u\n");
+        my $links = [];
+        if ($response && $response->{success}) {
+            $links = find_links($response, $u);
+        }
+        $forkman->finish(0, $links);
+    }
+    $forkman->wait_all_children;
+
+    for my $u (shuffle @uris) {
+        $forkman->start and next;
+        my $response = $url_archiver->get_remote($u);
+        $log->debug("[$$] CRAWL $response->{status} $u\n");
+        $forkman->finish;
+    }
+    $forkman->wait_all_children;
+}
+
+sub find_links {
+    my ($response, $uri) = @_;
+    return [] unless ( ($response->{headers}{"content-type"} //"") =~ m{^ text/html }x );
+
+    my $links = [];
+
+    my $base_uri = URI->new($uri);
+    my $dom = Mojo::DOM->new($response->{content});
+
+    my $x = $dom->find("a[href]")->grep(
+        sub {
+            return defined( $_->attr("href") );
+        }
+    )->map(
+        sub {
+            my $v = $_->attr("href");
+            $v =~ s/#.*$//;
+            return URI->new_abs($v, $base_uri);
+        }
+    )->grep(
+        sub {
+            $_->scheme =~ /\A https? \z/x;
+        }
+    );
+
+    @$links = $x->map(sub { "$_" })->uniq->each;
+    return $links;
+}
+
+1;
