24ed80f71da3f22ef292e77539c1639955fad19b
Kang-min Liu <gugod@gugod.org>
2015-11-18 18:51:53 +0100

    Limit the result to contain only urls created before the object.

--- lib/Diversion/UrlArchiveIterator.pm
+++ lib/Diversion/UrlArchiveIterator.pm
@@ -5,6 +5,11 @@ with 'Diversion::Db', 'Diversion::Iterator';
 
 use Diversion::UrlArchiver;
 
+has object_created_at => (
+    is => "ro",
+    default => sub { scalar time }
+);
+
 has sql_where_clause => (
     is => "ro",
     predicate => 1
@@ -25,14 +30,16 @@ sub reify {
             my ($dbh) = @_;
             my $SELECT_CLAUSE = "SELECT uri,sha1_digest,created_at FROM uri_archive";
             my $ORDER_CLAUSE = "ORDER BY created_at DESC";
-            my ($WHERE_CLAUSE, @where_values) = ("1=1");
+            my ($WHERE_CLAUSE, @where_values) = ("created_at < ?", $self->object_created_at);
 
             if ($self->has_sql_order_clause) {
                 $ORDER_CLAUSE = "ORDER BY " . $self->sql_order_clause;
             }
 
             if ($self->has_sql_where_clause) {
-                ($WHERE_CLAUSE, @where_values) = @{$self->sql_where_clause};
+                my @w = @{$self->sql_where_clause};
+                $WHERE_CLAUSE = "( $WHERE_CLAUSE ) AND ( " . shift(@w) . " )";
+                push @where_values, @w;
             }
 
             return $dbh->selectall_arrayref("$SELECT_CLAUSE WHERE $WHERE_CLAUSE $ORDER_CLAUSE LIMIT ?,1000", {Slice=>{}}, @where_values, $ext_cursor);
