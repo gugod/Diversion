a761928dda77238c497496017b3e430caca76c1c
Kang-min Liu <gugod@gugod.org>
2015-11-18 20:36:02 +0100

    Add a 'FeedUrlIterator'
    Just to iterate over all feed URLs.

---
+++ lib/Diversion/App/Command/feed.pm
@@ -0,0 +1,29 @@
+package Diversion::App::Command::feed;
+use v5.18;
+use Moo;
+with 'Diversion::AppRole', 'Diversion::Db';
+use Diversion::App -command;
+
+use Diversion::FeedUrlIterator;
+use JSON::XS;
+use URI::Split qw(uri_split);
+
+sub opt_spec {
+    return (
+    )
+}
+
+sub execute {
+    my ($self, $opt) = @_;
+    my $JSON = JSON::XS->new;
+    my $should_load_res = keys %$opt > 0;
+    my $last = "";
+    my $iter = Diversion::FeedUrlIterator->new();
+    my %aggs;
+
+    while (my $row = $iter->next) {
+        say $row->{uri};
+    }
+}
+
+1;
---
+++ lib/Diversion/FeedUrlIterator.pm
@@ -0,0 +1,39 @@
+package Diversion::FeedUrlIterator;
+use v5.18;
+use Moo;
+with 'Diversion::Iterator', 'Diversion::AppRole';
+
+use JSON::XS;
+use Diversion::UrlArchiveIterator;
+
+my $JSON = JSON::XS->new;
+
+has url_archive_iterator => (
+    is => "ro",
+    default => sub { Diversion::UrlArchiveIterator->new( sql_order_clause => "uri" ) }
+);
+
+sub reify {
+    my ($self) = @_;
+    my $rows = [];
+    my $iter = $self->url_archive_iterator;
+    my $last = "";
+    while ((@$rows < 100) && (my $row = $iter->next)) {
+        next unless $row->{uri} =~ /^https?:/ && $last ne $row->{uri};
+        my $blob = $self->blob_store->get($row->{sha1_digest});
+        unless (defined($blob)) {
+            warn "Blob is missing: $row->{sha1_digest} $row->{uri}";
+            next;
+        }
+        my $res = $JSON->decode( $blob );
+        next unless defined($res->{headers}{"content-type"}) && $res->{headers}{"content-type"} =~ /atom|rss/;
+
+        $last = $row->{uri};
+        push @$rows, $row;
+    }
+    $self->reified($rows);
+    $self->_cursor(0);
+    return $self;
+}
+
+1;
