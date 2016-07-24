70755a345b1842accb376f5cde9bc1eff5d88443
Kang-min Liu <gugod@gugod.org>
2016-07-24 17:39:46 +0200

    catch up with uri_archive v2 schema.

--- lib/Diversion/UrlArchiver.pm
+++ lib/Diversion/UrlArchiver.pm
@@ -15,18 +15,18 @@ package Diversion::UrlArchiver {
     sub get_local {
         my ($self, $url) = @_;
 
-        my $sha1_digest = $self->db_open(
+        my $response_sha1_digest = $self->db_open(
             "url",
             sub {
                 my ($dbh) = @_;
-                my ($v) = @{$dbh->selectcol_arrayref(q{ SELECT sha1_digest FROM uri_archive WHERE uri = ? ORDER BY created_at DESC LIMIT 1},{}, $url)};
+                my ($v) = @{$dbh->selectcol_arrayref(q{ SELECT response_sha1_digest FROM uri_archive WHERE uri = ? ORDER BY updated_at DESC LIMIT 1},{}, $url)};
                 return $v;
             }
         );
-        return undef unless defined $sha1_digest;
+        return undef unless defined $response_sha1_digest;
 
         my $blob_store = $self->blob_store;
-        if (my $response = $blob_store->get($sha1_digest)) {
+        if (my $response = $blob_store->get($response_sha1_digest)) {
             return $JSON->decode($response);
         }
         return undef;
@@ -38,21 +38,30 @@ package Diversion::UrlArchiver {
         my $blob_store = $self->blob_store;
 
         my $response = HTTP::Tiny->new( timeout => 60, max_size => 512000, (agent => $ENV{DIVERSION_HTTP_USER_AGENT} || "Diversion ") )->get($url);
-        if (is_binary($response->{content})) {
-            $response->{content} = { sha1_digest => $blob_store->put($response->{content}) };
-        }
+        my $response_content = delete $response->{content};
+        my $response_content_digest = $blob_store->put($response_content);
+
         my $response_digest = $blob_store->put("". $JSON->encode($response));
 
         $self->db_open(
             "url",
             sub {
                 my ($dbh) = @_;
-                my $sth_check = $dbh->prepare(q{ SELECT 1 FROM uri_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1});
-                $sth_check->execute($url, $response_digest);
-                unless ($sth_check->fetchrow_array) {
-                    my $sth_insert = $dbh->prepare(q{ INSERT INTO uri_archive(uri, created_at, sha1_digest) VALUES (?,?,?)});
-                    $sth_insert->execute($url, 0+time, $response_digest);
-                    $sth_insert->finish;
+                my $sth_check = $dbh->prepare(q{ SELECT 1 FROM uri_archive WHERE uri = ? AND content_sha1_digest = ? LIMIT 1});
+                $sth_check->execute($url, $response_content_digest);
+                my $now = 0+time;
+                if ($sth_check->fetchrow_array) {
+                    $dbh->do(
+                        q{ UPDATE uri_archive SET updated_at = ? WHERE uri = ? AND content_sha1_digest = ? },
+                        {},
+                        $now, $url, $response_content_digest
+                    );
+                } else {
+                    $dbh->do(
+                        q{ INSERT INTO uri_archive(uri, created_at, updated_at, response_sha1_digest, content_sha1_digest) VALUES (?,?,?,?,?) },
+                        {},
+                        $url, $now, $now, $response_digest, $response_content_digest
+                    );
                 }
                 $sth_check->finish;
             }
