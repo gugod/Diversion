36707301e5e2beb5b0846da32b46ad2a57fdcae5
Kang-min Liu <gugod@gugod.org>
2017-01-04 22:48:09 +0100

    a command to list newly archived URLs.

---
+++ lib/Diversion/App/Command/whatsnew.pm
@@ -0,0 +1,29 @@
+package Diversion::App::Command::whatsnew;
+#ABSTRACT: List new URLs in the archive
+use v5.18;
+use Diversion::App -command;
+
+use Moo;
+use DateTime;
+use DateTime::Format::MySQL;
+
+sub opt_spec {
+    return (
+        ["ago=i", "Include entries created up to this second ago.", { default => 86400 }],
+    )
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+    my $x = DateTime::Format::MySQL->format_datetime( DateTime->from_epoch( epoch => (time - $opt->{ago}) ) );
+    my $iter = Diversion::UrlArchiveIterator->new(
+	sql_where_clause => [
+	    "created_at > ?", $x
+	]
+    );
+    while (my $row = $iter->next) {
+	say $row->{uri};
+    }
+}
+
+1;
