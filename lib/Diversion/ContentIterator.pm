9754fd79f7be06d029a4248348328ac15949782b
Kang-min Liu <gugod@gugod.org>
2017-03-04 19:37:34 +0100

    ContentIterator - for "content" table.

---
+++ lib/Diversion/ContentIterator.pm
@@ -0,0 +1,57 @@
+package Diversion::ContentIterator;
+use v5.18;
+use Moo;
+with 'Diversion::Service', 'Diversion::Iterator';
+
+use DateTime;
+use DateTime::Format::MySQL;
+
+has sql_where_clause => (
+    is => "ro",
+    predicate => 1
+);
+
+has sql_order_clause => (
+    is => "ro",
+    predicate => 1
+);
+
+has _ext_cursor => (
+    is => "rw",
+    predicate => 1
+);
+
+sub reify {
+    my ($self) = @_;
+    my $ext_cursor = $self->_has_ext_cursor ? $self->_ext_cursor : 0;
+
+    my $rows = $self->db_open(
+        content => sub {
+            my ($dbh) = @_;
+            my $SELECT_CLAUSE = "SELECT uri,uri_content_sha1_digest,sha1_digest,created_at FROM content";
+            my $ORDER_CLAUSE = "";
+
+	    my $x = DateTime::Format::MySQL->format_datetime( DateTime->from_epoch( epoch => $self->object_created_at ) );
+            my ($WHERE_CLAUSE, @where_values) = ("created_at < ?", $x);
+
+            if ($self->has_sql_order_clause) {
+                $ORDER_CLAUSE = "ORDER BY " . $self->sql_order_clause;
+            }
+
+            if ($self->has_sql_where_clause) {
+                my @w = @{$self->sql_where_clause};
+                $WHERE_CLAUSE = "( $WHERE_CLAUSE ) AND ( " . shift(@w) . " )";
+                push @where_values, @w;
+            }
+
+            return $dbh->selectall_arrayref("$SELECT_CLAUSE WHERE $WHERE_CLAUSE $ORDER_CLAUSE LIMIT ?,1000", {Slice=>{}}, @where_values, $ext_cursor);
+        }
+    );
+
+    $self->reified($rows);
+    $self->_ext_cursor( $ext_cursor + @$rows );
+    $self->_cursor( 0 );
+    return $self;
+}
+
+1;
