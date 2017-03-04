523cf9a21baff4bb867f6875082a761cd38dfeeb
Kang-min Liu <gugod@gugod.org>
2017-03-04 20:59:30 +0100

    add mail_recent_content comand.

---
+++ lib/Diversion/App/Command/mail_recent_content.pm
@@ -0,0 +1,79 @@
+package Diversion::App::Command::mail_recent_content;
+# ABSTRACT: Iterate through recent content and email them.
+use v5.18;
+use Diversion::App -command;
+use Moo;
+with 'Diversion::Service';
+
+use Diversion::ContentIterator;
+
+use Encode;
+use JSON;
+
+use DateTime;
+use DateTime::Format::MySQL;
+
+sub opt_spec {
+    return (
+        ["to=s", "Send to this email address.", {}],
+	["limit=i", "Send this amount of email", { default => 10 }],
+    )
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+    my $JSON = JSON->new->utf8->canonical;
+
+    my $count = 0;
+    my $iter = Diversion::ContentIterator->new;
+    while (my $row = $iter->next) {
+	next if $row->{uri} =~ m{/$};
+
+        my $blob = $self->blob_store->get($row->{sha1_digest}) or next;
+        my $res;
+        eval {
+            $res = $JSON->decode($blob);
+            1;
+        } or do {
+            warn "Fail to decode json for of blob $row->{sha1_digest}\n";
+            next;
+        };
+
+	my $text_body = $res->{main_text};
+
+	next unless $text_body;
+	next unless length($text_body) > 300;
+	my $first_nl = index($text_body, "\n");
+	next unless $first_nl < 80 && $first_nl > 12;
+
+	my $subject = substr($text_body, 0, $first_nl);
+
+
+	$text_body = "Link: $row->{uri}\n\n" . $text_body;
+
+	$self->mail_this({
+	    subject => "#Diversion: $subject",
+	    text_body => $text_body,
+	    ($opt->{to} ? ( to => $opt->{to} ):()),
+        });
+
+	last if ($count++ > $opt->{limit});
+
+    }
+}
+
+sub mail_this {
+    my ($self, $mail_params) = @_;
+
+    my $config = $self->app->config;
+
+    my $email = Email::Stuffer->new($mail_params);
+    $email->transport("SMTP", $config->{smtp});
+    $email->from( $config->{email}{from} );
+
+    unless ($email->send()) {
+	say "Error sending this mail: " . $mail_params->{subject};
+    }
+}
+
+1;
---
+++ t/diversion-content-iterator.t
@@ -0,0 +1,25 @@
+use v5.18;
+use strict;
+
+use Diversion::App;
+use Diversion::ContentIterator;
+
+use File::XDG;
+use TOML qw(from_toml);
+
+my $xdg = File::XDG->new( name => "diversion" );
+
+my $config_file = $xdg->config_home->file("config.toml");
+my $config = from_toml(scalar $config_file->slurp);
+
+my $app = Diversion::App->new->set_config($config);
+
+
+
+
+
+my $iter = Diversion::ContentIterator->new();
+
+while (my $row = $iter->next()) {
+    say $row->{uri};
+}
