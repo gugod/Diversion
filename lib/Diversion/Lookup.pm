50587aeda76c6eed2e049b68bb22eaa502340979
Kang-min Liu <gugod@gugod.org>
2018-03-01 21:35:01 +0900

    migrate to new schema: `uri` column becomes `uri_id`

--- lib/Diversion/Lookup.pm
+++ lib/Diversion/Lookup.pm
@@ -8,6 +8,46 @@ has what => (
     required => 1,
 );
 
+sub bulk_lookup_by_id {
+    my ($self, $vals) = @_;
+    my @missing = @$vals;
+
+    my %lookup;
+
+    while (@missing) {
+        my $question_marks = join(',' , map { '?' } @missing);
+        my $what = $self->what;
+        my $table = "lookup_${what}";
+        my $sql_lookup = qq{ SELECT `id`,`${what}` FROM `${table}` WHERE `id` IN ($question_marks) };
+
+        $self->db_open(
+            "lookup",
+            sub {
+                my ($dbh) = @_;
+                my $rows = $dbh->selectall_arrayref($sql_lookup, {}, @missing);
+                for (@$rows) {
+                    $lookup{ $_->[0] } = $_->[1];
+                }
+            }
+        );
+
+        my @still_missing = grep { !defined($lookup{$_}) } @missing;
+
+        if (@still_missing) {
+            $self->db_open(
+                "lookup",
+                sub {
+                    my ($dbh) = @_;
+                    my $placeholder = join(', ' , map { '(?)' } @still_missing);
+                    $dbh->do(qq{ INSERT INTO $table (`$what`) VALUES ${placeholder} ON DUPLICATE KEY UPDATE id=id }, undef, @still_missing);
+                }
+            );
+        }
+        @missing = @still_missing;
+    }
+    return \%lookup;
+}
+
 sub bulk_lookup {
     my ($self, $vals) = @_;
     my @missing = @$vals;
--- lib/Diversion/UrlArchiveIterator.pm
+++ lib/Diversion/UrlArchiveIterator.pm
@@ -4,7 +4,19 @@ use Moo;
 use constant {
     database => "url",
     table => "uri_archive",
-    columns => [qw(uri response_sha1_digest content_sha1_digest created_at)]
+    columns => [qw(uri_id response_sha1_digest content_sha1_digest created_at)]
 };
-with 'Diversion::SingleTableIterator';
+with 'Diversion::SingleTableIterator', 'Diversion::Service';
+
+after 'reify' => sub {
+    my ($self) = @_;
+    my @uri_id = map { $_->{uri_id} } @{$self->reified};
+    my $lk = Diversion::Lookup->new( what => "uri" );
+    my $lookup = $lk->bulk_lookup_by_id(\@uri_id);
+    for (@{$self->reified}) {
+        $_->{uri} = $lookup->{$_->{uri_id}};
+    }
+};
+
+
 1;
