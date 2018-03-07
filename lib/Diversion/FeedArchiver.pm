a61228b3185dfeba85642c549d298615e1bdcddf
Kang-min Liu <gugod@gugod.org>
2018-03-08 06:06:41 +0900

    keep only db-dependent statements inside the db_open() block.

--- lib/Diversion/FeedArchiver.pm
+++ lib/Diversion/FeedArchiver.pm
@@ -22,14 +22,13 @@ package Diversion::FeedArchiver {
 
         my $JSON = JSON->new->canonical->utf8->pretty;
 
+        my $feed = Diversion::FeedFetcher->new(url => $url)->feed;
+        my $digest = $self->blob_store->put( "". $JSON->encode( $feed ) );
+
         $self->db_open(
             "feed",
             sub {
                 my ($dbh) = @_;
-
-                my $feed = Diversion::FeedFetcher->new(url => $url)->feed;
-                my $digest = $self->blob_store->put( "". $JSON->encode( $feed ) );
-
                 my ($exists) = @{ $dbh->selectcol_arrayref(q{ SELECT 1 FROM feed_archive WHERE uri = ? AND sha1_digest = ? LIMIT 1}) };
                 return if $exists;
                 $dbh->do(q{ INSERT INTO feed_archive(uri, created_at, sha1_digest) VALUES (?,?,?)}, {}, $url, 0+time, $digest);
